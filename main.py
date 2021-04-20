db7830ed9ff0fe37f0fac4a3c5db83fd47a32773
Wei226 <78838010+Wei226@users.noreply.github.com>
2021-04-20 16:34:49 -0700

    Update main.py
    update students info

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+# Author: Wei Huang, Walter Castillo, Nicholas Matthew Gomez
+# Semester Year: Spring 2021
+# CRN: 37239
+# Purpose: Allow two player to play Scrabble Game by entering words
+
 def inputRounds():
   x = float(input("Enter number of rounds: ")
