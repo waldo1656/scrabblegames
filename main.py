29b781572e6c80ad507bf5ec19d75a588f16cb6a
Wei226 <78838010+Wei226@users.noreply.github.com>
2021-04-20 16:52:35 -0700

    Update main.py
    update 1

--- main.py
+++ main.py
@@ -4,4 +4,12 @@
 # Purpose: Allow two player to play Scrabble Game by entering words
 
 def inputRounds():
-  x = float(input("Enter number of rounds: ")
+  x = int(input("Enter number of rounds: ")
+  if type(x)!=int
+     return x=2
+def inputWord(currPlayer,lastWordEntered,Player1)
+  currPlayer=int(input("Enter current player number 1 or 2")
+                 
+def validWord()
+   
+                 
